2d924bffd08388218abce2ab69c6ab2d90ecaeb8
zhangxilong <zhangxilong@fit-bean.com>
2025-05-10 11:13:26 +0800

    fix: 更新上传文件夹路径，调整为 'uploads/image'

--- app/config/config.py
+++ app/config/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-should-be-changed'
     DEBUG = False
-    UPLOAD_FOLDER = 'uploads'
+    UPLOAD_FOLDER = os.path.join('uploads', 'image')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 class DevelopmentConfig(Config):
