695b7d220059df45743a929013f6d11f03996392
feldan <feldan@foxmail.com>
2025-05-14 15:53:51 +0800

    feat: 在配置文件中新增TTS输出文件夹路径和API配置，更新开发及生产环境的API_KEY

--- app/config/config.py
+++ app/config/config.py
@@ -5,15 +5,20 @@ class BaseConfig:
     DEBUG = False
     UPLOAD_FOLDER = os.path.join('uploads', 'image')
     AUDIO_UPLOAD_FOLDER = os.path.join('uploads', 'audio')
+    TTS_OUTPUT_FOLDER = os.path.join('uploads', 'tts')
     ALLOWED_EXTENSIONS = {'jpg', 'mp3'}
     BASE_URL = 'http://172.26.25.10:5000'
-
+    
+    # API配置
+    API_URL = "https://ai.gitee.com/v1"
+    
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
+    API_KEY = "7CNIDO2WCXSMVYQXRVC2IDBUFGMGH1U0TFXLIQ1V"
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
-
+    API_KEY = "HIG3RE8ZBAIRKDJELB0D5E0YP1PEIKER2EMHAU1T"
 # 直接导出配置实例而不是类
 development_config = DevelopmentConfig()
 production_config = ProductionConfig()
